0e94deda14ccdce0c4c6465baf4c8d407951e62f
PatriciaMendozas <patriciamendozasoncco@gmail.com>
2016-12-21 10:23:14 -0500

    subo main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+var tiempo, costo, multiplicar;
+
+tiempo = prompt("Por favor, ingresa el tiempo por minuto");
+costo = prompt("Por favor, ingresa el costo por minuto ");
+
+resultado = parseFloat(tiempo) * parseFloat(costo);
+
+alert(resultado);
